a7120d31326e4a20111aa75bd7adf38cb8bad136
Ricardo Venieris <rvenieris@gmail.com>
2022-10-18 14:17:38 -0300

    Initial Commit

---
+++ TMDBListSwiftUI/TMDBListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBListSwiftUIApp.swift
+//  TMDBListSwiftUI
+//
+//  Created by Ricardo Venieris on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TMDBListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
